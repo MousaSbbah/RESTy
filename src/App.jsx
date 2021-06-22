9ab5801fe23fb87ff08d5c2e8a4ee7d6e508cd83
mousasbbah <mosassbbah@gmail.com>
2021-06-23 02:07:17 +0300

    try test

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,37 @@ import Header from './components/header/header';
 import Form from './components/form/form'; 
 import Footer from './components/footer/footer';
 import Result from './components/result/result';
+import History from './components/history/history';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state= {}
+    this.state= {history:[],loading:true,response:{type:'',data:{}}
+    }
 }
 
 handleForm = (data) =>{
-  this.setState(data)
+
+    this.setState({response:{data,type:'json'}});
+  
+}
+
+updateHistory = (data) =>{
+
+  this.setState({history:data});
+
+}
+
+loading = (data)=>{
+  this.setState({ loading:data});
+  console.log(data)
+}
+
+handleErrors = (data) =>{
+
+  this.setState({response:{data,type:'error'}});
+
 }
   render(){
 
@@ -21,8 +42,26 @@ handleForm = (data) =>{
       <React.Fragment>
         <Header />
         <main>
-        <Form  handler={this.handleForm}/>
-        <Result data = {this.state}/>
+        <dev className='left'>
+          <History>
+          {this.state.history.map(obj=>{
+            return (<div className='record'>
+              <dev className='method'>{obj.method}</dev>
+              <dev className='url'>{obj.URL}</dev>
+              </div>
+              )
+          })}
+        </History>
+
+          </dev>
+          <dev className='right'>
+          <Form loading={this.loading} handler={this.handleForm} updateHistory={this.updateHistory} errorHandler={this.handleErrors}/>
+        <Result loading={this.state.loading} data = {this.state.response}/>
+
+          </dev>
+
+        
+        
 
         </main>
         <Footer />
--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,34 +1,63 @@
 import "./form.scss";
 import React from "react";
-
+import {If,Else} from "../if/if";
 class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state={
         method:'',
         URL:'',
+        body:{},
+        history:[],
+        validBody:false
     }
   }
-
+  writeJSONCheck = async(e)=>{
+    try {
+      this.setState({body:JSON.parse(e.target.value)});
+      this.setState({validBody:true});
+    } catch (error) {
+      this.setState({validBody:false});
+    }
+  }
   submitHandler = async (e) => {
+    this.props.loading(false);
     e.preventDefault();
+    console.log(e.target);
+    console.log(e.target.body.value);
     let URL = e.target.urlText.value;
     let method = e.target.method.value;
     this.setState({ URL, method });
-    const raw = await fetch(URL,{
-      method: method,
-    })
-    let response = await raw.json();
-    let header ={}
-     await raw.headers.forEach((val,key)=>{
-      header[key]=val;
-    });
+    try {
+      if (!this.state.validBody && method !=='get') throw new Error ('syntax error :Incorrect JSON input in the body')
+      const raw = await fetch(URL,{
+        method: method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+          body: (method === 'get')? null : JSON.stringify(this.state.body)
+        })
+        if (!raw.ok) {
+          const message = `An error has occurred: ${raw.status}`;
+          throw new Error(message);
+        }
+      console.log(this.state);
+      let response = await raw.json();
+      let header ={}
+       await raw.headers.forEach((val,key)=>{
+        header[key]=val;
+      });
+      const newHistory = this.state.history;
+      newHistory.push({method,URL,body:this.state.body});
+      this.setState({ history:newHistory  });
+      this.props.handler({header,response});
+      this.props.updateHistory(this.state.history);
+      this.props.loading(true);
+    } catch (error) {
+      this.props.errorHandler(error);
+      this.props.loading(true);
+    }
 
-    this.props.handler({header,response});
-      
-    
-   
-    
   };
 
   render() {
@@ -50,6 +79,16 @@ class Form extends React.Component {
           <input type="radio" name="method" id="put" value="put" />
           <label htmlFor="put">PUT </label>
         </div>
+        <label htmlFor="body">BODY ( json )</label>
+         
+          <If condition={this.state.validBody}>
+            Correct Json
+            </If> 
+            <Else condition={this.state.validBody}>
+              incorrect JSON
+            </Else>
+            
+        <textarea  id="body" name="body" onChange={this.writeJSONCheck}></textarea>
       </form>
 
       <div className="request">
